65730224333f086342c59662f56b85421c641098
Jennifer Casto <CastoCustomCreations@gmail.com>
2024-02-09 14:07:56 -0500

    2 sections

---
+++ src/components/accordian/data.js
@@ -0,0 +1,32 @@
+const data = [
+    {
+        id: '1',
+        question: "What are accordion components?",
+        answer:
+            "Accordion components are user iinterface elements used for organizing data"
+
+    },
+    {
+        id: '2',
+        question: "What are they used for?",
+        answer:
+            "They are commonly employed in various contexts, including FAQs, product descriptions"
+
+    },
+    {
+        id: '3',
+        question: "What are accordion components?",
+        answer:
+            "The accordion is a musical instrument with a keyboard and bellows. it produces sound when squeezed"
+
+    },
+    {
+        id: '4',
+        question: "Can I create an accordion component with a different framework?",
+        answer:
+            "Yes of course, it is very possible to create an accordion component with another framework"
+
+    },
+
+];
+export default data;
